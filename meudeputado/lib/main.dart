56dddf073c6393d4e7f77a6a87b083ba7c592445
Luan Prates <pratesr21@gmail.com>
2020-01-25 18:40:08 -0300

    app flutter criado

---
+++ meudeputado/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+        body: ListaDeputados(),
+        appBar: AppBar(
+          title: Text('Deputados'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        )
+    )
+)
+);
+
+class ListaDeputados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemDeputado(Deputado('ACRÍSIO SENA', 1000)),
+        ItemDeputado(Deputado('TIN GOMES', 1234)),
+        ItemDeputado(Deputado('VITOR VALIM', 4312)),
+        ItemDeputado(Deputado('RENATO ROSENO', 5512)),
+      ],
+    );
+  }
+}
+
+class ItemDeputado extends StatelessWidget{
+
+  final Deputado _deputado;
+
+  ItemDeputado(this._deputado);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_deputado.nome.toString()),
+        subtitle: Text(_deputado.valor.toString()),
+
+      ),
+    );
+  }
+
+}
+
+class Deputado {
+
+  final String nome;
+  //final String descricao;
+  final double valor;
+
+
+  Deputado(this.nome, this.valor);
+}
